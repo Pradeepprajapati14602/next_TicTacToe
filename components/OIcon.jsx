ea58e86804269d55ace2ee407b1c98506c3e16f6
Pradeepkumar Prajapati <82260859+Pradeepprajapati14602@users.noreply.github.com>
2024-02-02 00:41:09 +0530

    update (#1)

---
+++ components/OIcon.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const OIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="feather feather-circle text-blue-500 h-5 w-5"
+    >
+      <circle cx={12} cy={12} r={10} />
+    </svg>
+  )
+}
